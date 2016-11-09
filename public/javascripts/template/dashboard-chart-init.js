dea0b638b00dc307b8c94cb7b8c39687760acc78
stary <yzx9610@outlook.com>
2016-11-09 22:43:14 +0800

    graph

--- public/javascripts/template/dashboard-chart-init.js
+++ public/javascripts/template/dashboard-chart-init.js
@@ -1,64 +1,14 @@
 // Use Morris.Area instead of Morris.Line
-Morris.Donut({
-    element: 'graph-donut',
-    data: [
-        {value: 40, label: 'New Visit', formatted: 'at least 70%' },
-        {value: 30, label: 'Unique Visits', formatted: 'approx. 15%' },
-        {value: 20, label: 'Bounce Rate', formatted: 'approx. 10%' },
-        {value: 10, label: 'Up Time', formatted: 'at most 99.99%' }
-    ],
-    backgroundColor: false,
-    labelColor: '#fff',
-    colors: [
-        '#4acacb','#6a8bc0','#5ab6df','#fe8676'
-    ],
-    formatter: function (x, data) { return data.formatted; }
-});
-
 
 $(function() {
-
-    var d1 = [
-        [0, 501],
-        [1, 620],
-        [2, 437],
-        [3, 361],
-        [4, 549],
-        [5, 618],
-        [6, 570],
-        [7, 758],
-        [8, 658],
-        [9, 538],
-        [10, 488]
-
-    ];
     var d2 = [
-        [0, 401],
-        [1, 520],
-        [2, 337],
-        [3, 261],
-        [4, 449],
-        [5, 518],
-        [6, 470],
-        [7, 658],
-        [8, 558],
-        [9, 438],
-        [10, 388]
+        [0, 98],
+        [1, 95],
+        [2, 43]
     ];
 
     var data = ([{
-        label: "New Visitors",
-        data: d1,
-        lines: {
-            show: true,
-            fill: true,
-            fillColor: {
-                colors: ["rgba(255,255,255,.4)", "rgba(183,236,240,.4)"]
-            }
-        }
-    },
-        {
-            label: "Unique Visitors",
+            label: "分数",
             data: d2,
             lines: {
                 show: true,
@@ -85,7 +35,7 @@ $(function() {
         // Tooltip
         tooltip: true,
         tooltipOpts: {
-            content: "%s X: %x Y: %y",
+            content: "%s: %y",
             shifts: {
                 x: -60,
                 y: 25
@@ -102,11 +52,6 @@ $(function() {
             shadowSize: 0,
             highlightColor: 'rgba(000,000,000,.2)'
         },
-//        lines: {
-//            show: true,
-//            fill: true
-//
-//        },
         points: {
             show: true,
             radius: 3,
@@ -114,5 +59,5 @@ $(function() {
         },
         colors: ["#5abcdf", "#ff8673"]
     };
-    var plot = $.plot($("#main-chart #main-chart-container"), data, options);
+    var plot = $.plot($("#main-chart #main-chart-score"), data, options);
 });
